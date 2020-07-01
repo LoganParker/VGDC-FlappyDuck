a2fda07c98ac82696251a0b42b6724abdf52a3f9
LoganParker <logandavidparker@gmail.com>
2020-06-30 19:21:03 -0600

    Quacky Bird V.10
    A true masterpiece

---
+++ Assets/Scripts/ClickController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ClickController : MonoBehaviour
+{
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+    
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+
+
+    public AudioSource flapAudio;
+    public AudioClip[] flapClips;
+    public int randomFlapSound;
+    public AudioSource scoreAudio;
+    public AudioClip[] scoreClips;
+    public int randomScoreSound;
+    public AudioSource dieAudio;
+    public AudioClip[] dieClips;
+    public int randomDieSound;
+
+    
+
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    GameManager game;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0,0,-90);
+        forwardRotation = Quaternion.Euler(0,0,35);
+        game = GameManager.Instance;
+        rigidbody.simulated = false;
+        flapClips = Resources.LoadAll<AudioClip>("FlapSounds");
+        scoreClips = Resources.LoadAll<AudioClip>("ScoreSounds");
+        dieClips = Resources.LoadAll<AudioClip>("CrashSounds");
+    }
+
+    void OnEnable() {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;    
+    }
+    void OnDisable() {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;    
+        
+    }
+    void OnGameStarted(){
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+    void OnGameOverConfirmed(){
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(game.GameOver) return;
+        if(Input.GetMouseButtonDown(0)){
+            randomFlapSound = Random.Range(0,2);
+            flapAudio.PlayOneShot(flapClips[randomFlapSound]);
+            
+            flapAudio.Play();
+            transform.rotation=forwardRotation;
+            rigidbody.velocity= Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce,ForceMode2D.Force);
+        }
+
+
+        transform.rotation= Quaternion.Lerp(transform.rotation,downRotation,tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.gameObject.tag == "ScoreZone"){
+            //register score event
+            OnPlayerScored(); // event sent to GameManager
+            //play sound if not lazy
+            randomScoreSound = Random.Range(0,2);
+            scoreAudio.PlayOneShot(scoreClips[randomScoreSound]);
+
+        }
+        if(collider.gameObject.tag == "DeadZone"){
+            rigidbody.simulated = false;
+            //register dead event
+            OnPlayerDied(); // Event sent to GameManager
+            randomDieSound = Random.Range(0,3);
+            dieAudio.PlayOneShot(dieClips[randomDieSound]);
+            
+            
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countDownPage;
+    public int Score { get{return score;} } 
+    public Text scoreText;
+
+    enum PageState{
+        None,
+        Start,
+        GameOver,
+        CountDown
+    }
+    int score = 0;
+    bool gameOver = true;
+
+    public bool GameOver { get{return gameOver;} }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+        
+    }
+    void OnEnable() {
+        CountdownText.OnCountdownFinished += OnCountdownFinished;
+        ClickController.OnPlayerDied += OnPlayerDied;      
+        ClickController.OnPlayerScored += OnPlayerScored;  
+    }
+    void OnDisable() {
+        CountdownText.OnCountdownFinished -= OnCountdownFinished;
+        ClickController.OnPlayerDied -= OnPlayerDied;      
+        ClickController.OnPlayerScored -= OnPlayerScored;
+    }
+    void OnCountdownFinished(){
+        SetPageState(PageState.None);
+        OnGameStarted();
+        score = 0;
+        gameOver = false;
+    }
+    void OnPlayerDied(){
+        gameOver = true;
+        int savedScore = PlayerPrefs.GetInt("HighScore");
+        if(score>savedScore){
+            PlayerPrefs.SetInt("HighScore",score);
+        }
+        SetPageState(PageState.GameOver);
+    }
+    void OnPlayerScored(){
+        score++;
+        scoreText.text = score.ToString();
+    }
+    // Update is called once per frame
+    void SetPageState(PageState state)
+    {
+        switch(state){
+            case PageState.None:
+                
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(false);
+            
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countDownPage.SetActive(false);
+                break;
+            case PageState.CountDown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(true);
+                break;
+        }
+    }
+    public void ConfirmGameOver(){
+        //Activated on Replay button
+        OnGameOverConfirmed(); //event
+        scoreText.text = "0";
+        SetPageState(PageState.Start);
+    }
+    public void StartGame(){
+        //activated on Play button
+        SetPageState(PageState.CountDown);
+    }
+
+
+
+}
